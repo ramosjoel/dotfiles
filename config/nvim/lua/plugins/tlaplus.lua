cbcdee0e322138520ac4864e02c63be48b911c8c
Joel <7258285+ramosjoel@users.noreply.github.com>
2026-02-20 08:52:15 -0800

    Adds TLA+ plugin and model checker config

--- config/nvim/lua/plugins/tlaplus.lua
+++ config/nvim/lua/plugins/tlaplus.lua
@@ -1,10 +1,13 @@
 -- https://github.com/susliko/tla.nvim
 return {
-  "susliko/tla.nvim",
-  config = function ()
-    require("tla") .setup()
-  end,
-  keys = {
-    { "<leader>tc", function () require('tla').check() end, desc = "Run TLA+ Model Checker" },
+  {
+    "susliko/tla.nvim",
+    config = function()
+      require("tla").setup()
+    end,
+    keys = {
+      { "<leader>tc", function() require('tla').check() end, desc = "Run TLA+ Model Checker" },
+    },
   },
+  "tlaplus-community/tlaplus-nvim-plugin"
 }
